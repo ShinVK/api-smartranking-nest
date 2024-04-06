e556b3c7bec577ae9ac9ed2c9af1c63b136fc811
shinvk <victor.kamiguchi@gmail.com>
2024-04-06 01:56:46 -0300

    feat: implements jogadores controller, interface, dto

---
+++ src/jogadores/jogadores.controller.ts
@@ -0,0 +1,12 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CriarJogadorDto } from './dtos/criar-jogador.dto';
+
+@Controller('api/v1/jogadores')
+export class JogadoresController {
+  @Post()
+  async criarAtualizarJogador(@Body() criarJogadorDto: CriarJogadorDto) {
+    return JSON.stringify({
+      nome: criarJogadorDto.email,
+    });
+  }
+}
